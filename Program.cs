32df677c845909480b096046c26a8f51a9ddd10a
Coca162 <coca16622@gmail.com>
2020-11-19 19:06:02 +0000

    Initial code

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using SpookVooper.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SVNetworth
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Enter SV Username which you want networth for:");
+            string SVID = await SpookVooperAPI.Users.GetSVIDFromUsername(Console.ReadLine());
+            await Task.Delay(100);
+
+            string[] tickers = { "B", "IDE", "NEWS", "POT", "TECH", "TYCO", "VC", "VNB", "VU", "X" };
+
+            List<decimal> stock_values = new List<decimal>();
+
+            foreach (var ticker in tickers)
+            {
+                var OwnerData = await SpookVooperAPI.Economy.GetOwnerData(ticker);
+                await Task.Delay(100);
+                foreach (var owner in OwnerData)
+                {
+                    if (owner.Owner_Id == SVID)
+                    {
+                        decimal value = await SpookVooperAPI.Economy.GetStockBuyPrice(ticker);
+                        Console.WriteLine(value * owner.Amount);
+                        stock_values.Add(value * owner.Amount);
+                        await Task.Delay(100);
+                    }
+                }
+            }
+
+            Console.WriteLine($"Networth: ¢{stock_values.Sum()}");    
+        }
+    }
+}
---
+++ SpookVooper.Api/SpookVooper.Api/Stocks/OwnerData.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace SpookVooper.Api.Stocks
+{
+    public class OwnerData
+    {
+        [Key]
+        [JsonProperty("ownerId")] 
+        public string Owner_Id { get; set; }
+
+        [JsonProperty("ownerName")]
+        public string Owner_Name { get; set; }
+
+        [JsonProperty("amount")]
+        public int Amount { get; set; }
+    }
+}
